d12a049b387cf0c45e65b3c999e89122bba76e22
mehdi mirabian <mehdi mirabian>
2016-11-13 18:22:07 +0000

    adding new files

---
+++ controllers/api.py
@@ -0,0 +1,40 @@
+import random
+
+def index():
+    pass
+
+# Mocks implementation.
+def get_info():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    # We just generate a lot of of data.
+    info = []
+    has_more = False
+    rows = db().select(db.info.ALL, limitby=(start_idx, end_idx + 1))
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            t = dict(
+                id = r.id,
+                skills = r.skills,
+                available_times = r.available_times,
+            )
+            info.append(t)
+        else:
+            has_more = True
+    logged_in = auth.user_id is not None
+    return response.json(dict(
+        info=info,
+        logged_in=logged_in,
+        has_more=has_more,
+    ))
+
+@auth.requires_signature()
+def add_info():
+    t_id = db.info.insert(
+        skills = request.vars.skills,
+        available_times = request.vars.available_times
+    )
+    t = db.info(t_id)
+    return response.json(dict(info=t))
+
+
---
+++ static/js/default_index.js
@@ -0,0 +1,68 @@
+// This is the js for the default/index.html view.
+
+var app = function() {
+
+    var self = {};
+
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+
+
+
+
+
+    self.add_info_button = function () {
+        // The button to add a track has been pressed.
+        self.vue.is_adding_info = !self.vue.is_adding_info;
+    };
+
+        self.add_info = function () {
+        // The submit button to add a track has been added.
+        $.post(add_info_url,
+            {
+                skills: self.vue.form_skills,
+                available_times: self.vue.form_available_times
+            },
+            function (data) {
+                $.web2py.enableElement($("#add_info_submit"));
+                self.vue.info.unshift(data.info);
+            });
+    };
+
+
+
+    self.vue = new Vue({
+        el: "#vue-div",
+        data: {
+            is_adding_info: false,
+            info: [],
+            logged_in: false,
+            form_skills: null,
+            form_available_times: null,
+        },
+        methods: {
+            add_info_button: self.add_info_button,
+            add_info: self.add_info,
+
+        }
+
+    });
+
+    $("#vue-div").show();
+
+
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){APP = app();});
