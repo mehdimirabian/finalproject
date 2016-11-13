45a7da467f940ec2341831c9ba809b4c650c0a28
Junaid Ahmed <junaidahmed@Junaids-MacBook-Pro.local>
2016-11-13 14:45:52 -0800

    table change (added new table)

--- models/tables.py
+++ models/tables.py
@@ -7,11 +7,17 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
-db.define_table('info',
+db.define_table('profile',
+                Field('id', 'integer'),
                 Field('user_email', default=auth.user.email if auth.user_id else None),
-                Field('skills', 'text'),
-                Field('available_times', 'text')
+                Field('available_times', 'text'),
+                Field('photo', 'upload'),
+                Field('contact', 'integer')
                 )
+
+db.define_table('skill',
+                Field('id', 'integer'),
+                Field('skill', 'text'))
 
 # I don't want to display the user email by default in all forms.
 db.info.user_email.readable = db.info.user_email.writable = False
