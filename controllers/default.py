efd0e7036c04cc9016ba5eb3a4dd67f7ff4f8807
Junaid Ahmed <junaidahmed@dhcp-169-233-167-185.cruznet.ucsc.edu>
2016-11-28 16:30:29 -0800

    added profile_view.html and respective function in default.py. Profile view from grid works now

--- controllers/default.py
+++ controllers/default.py
@@ -9,70 +9,85 @@
 # -------------------------------------------------------------------------
 
 def get_user_name_from_email(email):
-    """Returns a string corresponding to the user first and last names,
+	"""Returns a string corresponding to the user first and last names,
     given the user email."""
-    u = db(db.auth_user.email == email).select().first()
-    if u is None:
-        return 'None'
-    else:
-        return ' '.join([u.first_name, u.last_name])
+	u = db(db.auth_user.email == email).select().first()
+	if u is None:
+		return 'None'
+	else:
+		return ' '.join([u.first_name, u.last_name])
+
 
 @auth.requires_login()
 def index():
-    # info = None
-    # names = []
-    # if auth.user_id is not None:
-    #     info = db(db.info.user_email == auth.user.email).select(db.info.ALL)
-    #     for i in info:
-    #         names.append(get_user_name_from_email(info.user_email))
-    q = db.info  # This queries for all products.
-    export_classes = dict(csv=False, json=False, html=False,
-                          tsv=False, xml=False, csv_with_hidden_cols=False,
-                          tsv_with_hidden_cols=False)
-    # links = [
-    #     lambda row: A('Edit', _class='button btn btn-default', _href=URL("default", "edit", args=[row.id]))]
-    selectable = lambda ids: delete(ids)
-    form = SQLFORM.grid(
-        q,
-        editable=False,
-        create=True,
-        selectable=selectable,
-        user_signature=False,
-        deletable=False,
-        fields=[db.info.first_name, db.info.last_name, db.info.user_email, db.info.skills, db.info.available_times,
-                ],
-        details=True,
-        exportclasses=export_classes
-    )
-    # my_extra_element = TR(LABEL("Don't view"),
-    #                       INPUT(_name='Unviewable', value=False, _type='checkbox'))
-    # form[1].insert(0, my_extra_element)
-    # row = db().select(db.info.ALL)
-    # if 'edit' in request.args:
-    #     if auth.user.email == row[0].user_email:
-    #         response.flash = T("emails are the same")
-    return dict(form=form)
+	# info = None
+	# names = []
+	# if auth.user_id is not None:
+	#     info = db(db.info.user_email == auth.user.email).select(db.info.ALL)
+	#     for i in info:
+	#         names.append(get_user_name_from_email(info.user_email))
+	q = db.info  # This queries for all products.
+	export_classes = dict(csv=False, json=False, html=False,
+						  tsv=False, xml=False, csv_with_hidden_cols=False,
+						  tsv_with_hidden_cols=False)
+	links = [lambda row: A('View Profile', _href=URL("default", "profile_view", args=[row.id]))]
+	selectable = lambda ids: delete(ids)
+	form = SQLFORM.grid(
+		q,
+		editable=True,
+		create=True,
+		selectable=selectable,
+		user_signature=False,
+		deletable=False,
+		fields=[db.info.first_name, db.info.last_name, db.info.user_email, db.info.skills, db.info.available_times,
+				],
+		details=True,
+		links=links,
+		exportclasses=export_classes
+	)
+	# my_extra_element = TR(LABEL("Don't view"),
+	#                       INPUT(_name='Unviewable', value=False, _type='checkbox'))
+	# form[1].insert(0, my_extra_element)
+	# row = db().select(db.info.ALL)
+	# if 'edit' in request.args:
+	#     if auth.user.email == row[0].user_email:
+	#         response.flash = T("emails are the same")
+	return dict(form=form)
+
 
 @auth.requires_login()
 def delete(ids):
-    row = db().select(db.info.ALL)
-    if row[0].user_email == auth.user.email:
-        to_delete = db(db.info.id.belongs(ids))
-        to_delete.delete()
-
+	to_delete = db(db.info.id.belongs(ids))
+	to_delete.delete()
 
 
 @auth.requires_login()
 def edit():
-    """
+	"""
     This is the page to create / edit / delete a post.
     """
-    # edit_item = request.args(0) or redirect(URL('index'))
-    # form = crud.update(db.info, edit_item, next='index')
+	user_id = request.args(0) or redirect(URL('index'))
+	form = SQLFORM(db.auth_user, user_id).process()
+	membership_panel = LOAD(request.controller,
+							'edit.html',
+							args=[user_id],
+							ajax=True)
+	return dict(form=form, membership_panel=membership_panel)
+
+
+# edit_item = request.args(0) or redirect(URL('index'))
+# form = crud.update(db.info, edit_item, next='index')
+#
+def profile_view():
+    profile = db(db.auth_user.id==auth.user_id).select(db.auth_user.id, db.auth_user.first_name, db.auth_user.last_name, db.auth_user.image).first()
+    table = db(db.info.user_email==auth.user.email).select(db.info.skills, db.info.available_times).first()
+    print(table)
+    return dict(profile=profile, table=table)
+
 
 
 def user():
-    """
+	"""
     exposes:
     http://..../[app]/default/user/login
     http://..../[app]/default/user/logout
@@ -87,30 +102,28 @@ def user():
     to decorate functions that need access control
     also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
     """
-    if request.args(0) == 'profile':
-        db.auth_user.first_name.writable = db.auth_user.last_name.writable = db.auth_user.email.writable = False
-        for field in auth.settings.extra_fields['auth_user']:
-            field.readable = True
-            field.writable = False
-    return dict(form=auth())
+	if request.args(0) == 'profile':
+		db.auth_user.first_name.writable = db.auth_user.last_name.writable = db.auth_user.email.writable = False
+		for field in auth.settings.extra_fields['auth_user']:
+			field.readable = True
+			field.writable = False
+	return dict(form=auth())
 
 
 @cache.action()
 def download():
-    """
+	"""
     allows downloading of uploaded files
     http://..../[app]/default/download/[filename]
     """
-    return response.download(request, db)
+	return response.download(request, db)
 
 
 def call():
-    """
+	"""
     exposes services. for example:
     http://..../[app]/default/call/jsonrpc
     decorate with @services.jsonrpc the functions to expose
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
-    return service()
-
-
+	return service()
